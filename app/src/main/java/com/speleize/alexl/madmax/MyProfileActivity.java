9572409f508b8c669290db15581b7dfacfa4b3aa
Unknown <robin.bigeard@gmail.com>
2018-12-29 19:11:17 +0100

    Profile log.i

--- app/src/main/java/com/speleize/alexl/madmax/MyProfileActivity.java
+++ app/src/main/java/com/speleize/alexl/madmax/MyProfileActivity.java
@@ -2,6 +2,9 @@ package com.speleize.alexl.madmax;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyProfileActivity extends AppCompatActivity {
 
@@ -9,6 +12,18 @@ public class MyProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+    }
+
+    public void addProfile(View view) {
+
+        EditText profilLastName = findViewById(R.id.profilLastName);
+        EditText profilFirstName = findViewById(R.id.profilFirstName);
+        EditText profilDate = findViewById(R.id.profilDate);
+
+        Log.i("Bigeard", String.valueOf(profilLastName.getText()));
+        Log.i("Bigeard", String.valueOf(profilFirstName.getText()));
+        Log.i("Bigeard", String.valueOf(profilDate.getText()));
 
     }
 }
